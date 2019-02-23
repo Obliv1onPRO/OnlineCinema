c4e01ed4b690fb0c97d00d028335d9549e4b6d44
obliv1on <ivan.peshekhonov@gmail.com>
2019-02-24 02:23:17 +0300

    Add interactive form changing

--- api/static/js/create.js
+++ api/static/js/create.js
@@ -1,3 +1,18 @@
 $(function () {
   $('[data-toggle="popover"]').popover()
-})
+});
+
+$("#room_type").change(function () {
+  if(!$("#room_type")[0].checked){
+    $("#invite_label").css({'display' : 'none'});
+    for (let i = 0; i < $(".invite").length; i++) {
+       $($(".invite")[i]).css({'display' : 'none'})
+    }
+  }
+  else {
+    $("#invite_label").css({'display' : 'flex'});
+    for (let i = 0; i < $(".invite").length; i++) {
+      $($(".invite")[i]).css({'display' : 'block'})
+    }
+  }
+});
