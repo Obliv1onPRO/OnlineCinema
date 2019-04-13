4267cb7819170527948240e46dc6d03f1466aa62
obliv1on <ivan.peshekhonov@gmail.com>
2019-04-13 12:22:22 +0300

    Setup admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from api.models import Profile, Room
+
+admin.site.site_header = 'Администрирование Online Cinema'
+admin.site.site_title = 'Административный сайт Online Cinema'
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'standart_user_id',
+        'login',
+        'email',
+        'current_room_id'
+    )
+    search_fields = (
+        'login',
+        'email',
+    )
+    readonly_fields = (
+        'standart_user_id',
+        'login',
+        'email',
+    )
+    exclude = (
+        'password',
+    )
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'type',
+        'creator',
+        'created_at'
+    )
+    search_fields = (
+        'id',
+        'name',
+        'creator'
+    )
+    readonly_fields = (
+        'id',
+        'name',
+        'type',
+        'creator',
+        'created_at'
+    )
